c514d0da339e401cc4b932cbca38543952b1892f
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-02-16 23:03:02 +0530

    data types added

---
+++ Typescript_fundamentals/dataTypes.ts
@@ -0,0 +1,9 @@
+//String Data types
+let fullName:string='Pawan yadav'
+console.log(fullName) // "pawan yadav"
+// Boolean Data type
+let isTrue:boolean=false
+console.log(isTrue) //false
+//Number Data type
+let myNumber:number=10
+console.log(myNumber) // 10
