718836a8aeb1e59c59433374f442c55eabbd0e83
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-02-17 22:43:23 +0530

    Array basics and defination

---
+++ Typescript_fundamentals/array.ts
@@ -0,0 +1,7 @@
+//An array is a collection of items of same data type stored at contiguous memory locations
+//These are treated as Objects in javascrpit/typescript and are passed by refference
+const myArr1: number[] = [12, 90, 71]; //Array of type number
+console.log(myArr1) // O/P:- [ 12, 90, 71 ]
+// another way of creating array
+const myArr2: Array<number> = [12, 90, 71];
+console.log(myArr2) //O/P:- [ 12, 90, 71 ]
