17ded7824308e3fdfb46b2819de46a11667f33fa
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-02-18 22:08:53 +0530

    Removed comments

--- Typescript_fundamentals/array.ts
+++ Typescript_fundamentals/array.ts
@@ -1,5 +1,3 @@
-//An array is a collection of items of same data type stored at contiguous memory locations
-//These are treated as Objects in javascrpit/typescript and are passed by refference
 const myArr1: number[] = [12, 90, 71]; //Array of type number
 console.log(myArr1) // O/P:- [ 12, 90, 71 ]
 // another way of creating array
