cc86897b07fd888fbcb9eaa17edc2bcaf8a27d37
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-02-22 13:06:40 +0530

    enums exampple 3  added

---
+++ Typescript_fundamentals/enum2.ts
@@ -0,0 +1,16 @@
+enum myCurrentBoyfriend {
+  monday = "Daniels",
+  teusday = "Neeraj",
+  wednesday = "Kunal chandra",
+  thursday = "Ravi",
+  friday = "Pratik",
+  saturday = "off day",
+  sunday = "fun day",
+}
+//?A girl from South Delhi is choosing her boyfriend
+console.log(`My todays boyfriend will be`, myCurrentBoyfriend.monday);
+//My todays boyfriend will be Daniels
+console.log(`My todays boyfriend will be`, myCurrentBoyfriend.teusday);
+//My todays boyfriend will be Neeraj
+console.log(`My todays boyfriend will be`, myCurrentBoyfriend.friday);
+//My todays boyfriend will be Pratik
